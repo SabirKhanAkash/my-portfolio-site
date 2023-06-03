51c04313608f1d62e4f4e5016735ab71b475386a
Sabir Khan Akash <4175.akash@gmail.com>
2023-06-03 22:51:56 +0600

    reformatted with prettier

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # My Personal Portfolio Site
+
 This is my personal portfolio site based on Glassmorphism UI. You are cordially welcomed to pay a visit in case you have a spare time. Have a good day.
 
 # Site Link: https://akash-codes.vercel.app/
+
 # Mirror Link: https://akash-codes.netlify.app/
 
 I have used `HTML`, `CSS`, `JavaScript`, [`Vanila-tilt.js`](https://micku7zu.github.io/vanilla-tilt.js/), [`Particles.js`](https://github.com/VincentGarreau/particles.js/) to build the site and for the deployment [`Vercel`](https://vercel.com/) was used primarily and [`Netlify`](https://www.netlify.com/) was used as a mirror site. THANK YOU.
--- js/script.js
+++ js/script.js
@@ -1,117 +1,120 @@
-window.addEventListener("load", () =>{
+window.addEventListener("load", () => {
   document.querySelector(".main").classList.remove("hidden");
   document.querySelector(".home-section").classList.add("active");
 
   document.querySelector(".page-loader").classList.add("fade-out");
-  setTimeout(() =>{
+  setTimeout(() => {
     document.querySelector(".page-loader").style.display = "none";
-  },600);
+  }, 600);
 });
 
 const navToggler = document.querySelector(".nav-toggler");
-navToggler.addEventListener("click", () =>{
-    console.log("Ho");
-    hideSection();
-    toggleNavbar();
-    document.body.classList.toggle("hide-scrolling");
+navToggler.addEventListener("click", () => {
+  console.log("Ho");
+  hideSection();
+  toggleNavbar();
+  document.body.classList.toggle("hide-scrolling");
 });
 
-function hideSection(){
-    document.querySelector("section.active").classList.toggle("fade-out");
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
 }
 
-function toggleNavbar(){
-    document.querySelector(".header").classList.toggle("active");
+function toggleNavbar() {
+  document.querySelector(".header").classList.toggle("active");
 }
 
-document.addEventListener("click", (e) =>{
-    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
-        document.querySelector(".overlay").classList.add("active");
-        navToggler.classList.add("hide");
-        const hash = e.target.hash;
-        console.log(hash);
-        if(e.target.classList.contains("nav-item")){
-            console.log(hash);
-            toggleNavbar();
-            // console.log("Achhi bhai");
-        }
-        else{
-            console.log("Nai bhai");
-        }
-        setTimeout(() =>{
-            document.querySelector("section.active").classList.remove("active","fade-out");
-            document.querySelector(e.target.hash).classList.add("active");
-            window.scrollTo(0,0);
-            document.body.classList.remove("hide-scrolling");
-            navToggler.classList.remove("hide");
-            document.querySelector(".overlay").classList.remove("active");
-        },500);
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    document.querySelector(".overlay").classList.add("active");
+    navToggler.classList.add("hide");
+    const hash = e.target.hash;
+    console.log(hash);
+    if (e.target.classList.contains("nav-item")) {
+      console.log(hash);
+      toggleNavbar();
+      // console.log("Achhi bhai");
+    } else {
+      console.log("Nai bhai");
     }
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
 });
 
+const textDisplay = document.getElementById("text");
+const phrases = [
+  " Software Developer.",
+  " Web Developer.",
+  " Problem Solver.",
+  " Gamer.",
+  " Tech Enthusiast.",
+  " Learner.",
+];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
 
-const textDisplay = document.getElementById('text')
-const phrases = [' Software Developer.', ' Web Developer.', ' Problem Solver.', ' Gamer.', ' Tech Enthusiast.', ' Learner.']
-let i = 0
-let j = 0 
-let currentPhrase = []
-let isDeleting = false
-let isEnd = false
-
-function loop () {
-  isEnd = false
-  textDisplay.innerHTML = currentPhrase.join('')
+function loop() {
+  isEnd = false;
+  textDisplay.innerHTML = currentPhrase.join("");
 
   if (i < phrases.length) {
-
     if (!isDeleting && j <= phrases[i].length) {
-      currentPhrase.push(phrases[i][j])
-      j++
-      textDisplay.innerHTML = currentPhrase.join('')
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      textDisplay.innerHTML = currentPhrase.join("");
     }
 
-    if(isDeleting && j <= phrases[i].length) {
-      currentPhrase.pop(phrases[i][j])
-      j--
-      textDisplay.innerHTML = currentPhrase.join('')
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j]);
+      j--;
+      textDisplay.innerHTML = currentPhrase.join("");
     }
 
     if (j == phrases[i].length) {
-      isEnd = true
-      isDeleting = true
+      isEnd = true;
+      isDeleting = true;
     }
 
     if (isDeleting && j === 0) {
-      currentPhrase = []
-      isDeleting = false
-      i++
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
       if (i === phrases.length) {
-        i = 0
+        i = 0;
       }
     }
   }
-  
-  const normalSpeed = Math.random() * (300 -299) + 35;
-  const time = isEnd ? 800 : isDeleting ? normalSpeed : normalSpeed
-  setTimeout(loop, time)
+
+  const normalSpeed = Math.random() * (300 - 299) + 35;
+  const time = isEnd ? 800 : isDeleting ? normalSpeed : normalSpeed;
+  setTimeout(loop, time);
 }
 
-loop()
-  
+loop();
 
-function message(){
-  const success = document.getElementById('success');
-
+function message() {
+  const success = document.getElementById("success");
 
   setTimeout(() => {
-      Name.value = '';
-      email.value = '';
-      msg.value = '';
+    Name.value = "";
+    email.value = "";
+    msg.value = "";
   }, 2000);
 
-  success.style.display = 'block';
+  success.style.display = "block";
   setTimeout(() => {
-      success.style.display = 'none';
+    success.style.display = "none";
   }, 4000);
-
 }
